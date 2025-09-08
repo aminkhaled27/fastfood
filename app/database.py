cce3bc21a80ffdad36ce75a6da2e9d5527510b85
AminKh27 <amin.kh.ragab@gmail.com>
2025-09-08 22:59:23 +0300

    ..

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/fastfood"
+SQLALCHEMY_DATABASE_URL = "psql 'postgresql://neondb_owner:npg_7kFQ8VyTgmPh@ep-calm-sunset-adkilvcl-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
